a47a838bd48bdd2ede89ef5dbf6fe4b9da21be4c
gainchang <gainchang620@gmail.com>
2022-05-03 01:33:51 +0900

    feat: Add movie router

---
+++ internal/controller/http/v1/error.go
@@ -0,0 +1,18 @@
+package v1
+
+// TODO: Implement Custom Error Wrapping
+//type response struct {
+//	Error string `json:"error" example:"message"`
+//}
+//
+//type CustomError interface {
+//}
+//
+//type serverError struct {
+//	Code    int    `json:"code" example:"501"`
+//	Message string `json:"error" example:"error_message"`
+//}
+//
+//func errorResponse(c *gin.Context, customError CustomError) {
+//	c.AbortWithStatusJSON(customError.Code, response{customError.Message})
+//}
