50695ae75c74ab358edd9008e5c5d4b4fbda3f0f
gainchang <gainchang620@gmail.com>
2022-05-06 19:26:08 +0900

    feat: Add left codes

--- internal/usecase/movie.go
+++ internal/usecase/movie.go
@@ -1,30 +1,30 @@
 package usecase
 
-import (
-	"context"
-	"fmt"
-)
-
-// MovieUseCase -.
-// separate to entity?
-type MovieUseCase interface {
-	Translate(context.Context, entity.Translation) (entity.Translation, error)
-	History(context.Context) ([]entity.Translation, error)
-}
-
-// MovieUseCase -
-type movieUseCase struct {
-	//repo   MovieRepo
-	webAPI MovieWebAPI
-}
-
-// NewMovieUseCase -
-func NewMovieUseCase(w MovieWebAPI) *MovieUseCase {
-	return &movieUseCase{
-		//repo:   r,
-		webAPI: w,
-	}
-}
+//
+//import (
+//	"context"
+//)
+//
+//// MovieUseCase -.
+//// separate to entity?
+//type MovieUseCase interface {
+//	Translate(context.Context, entity.Translation) (entity.Translation, error)
+//	History(context.Context) ([]entity.Translation, error)
+//}
+//
+//// MovieUseCase -
+//type movieUseCase struct {
+//	//repo   MovieRepo
+//	webAPI MovieWebAPI
+//}
+//
+//// NewMovieUseCase -
+//func NewMovieUseCase(w MovieWebAPI) *MovieUseCase {
+//	return &movieUseCase{
+//		//repo:   r,
+//		webAPI: w,
+//	}
+//}
 
 //// History - getting translate history from store.
 //func (uc *movieUseCase) History(ctx context.Context) ([]entity.Translation, error) {
@@ -51,17 +51,17 @@ func NewMovieUseCase(w MovieWebAPI) *MovieUseCase {
 //	return translation, nil
 //}
 
-// GetScore -.
-func (uc *movieUseCase) GetScore(ctx context.Context, t entity.Translation) (entity.Translation, error) {
-	translation, err := uc.webAPI.Translate(t)
-	if err != nil {
-		return entity.Translation{}, fmt.Errorf("TranslationUseCase - Translate - s.webAPI.Translate: %w", err)
-	}
-
-	err = uc.repo.Store(context.Background(), translation)
-	if err != nil {
-		return entity.Translation{}, fmt.Errorf("TranslationUseCase - Translate - s.repo.Store: %w", err)
-	}
-
-	return translation, nil
-}
+//// GetScore -.
+//func (uc *movieUseCase) GetScore(ctx context.Context, t entity.Translation) (entity.Translation, error) {
+//	translation, err := uc.webAPI.Translate(t)
+//	if err != nil {
+//		return entity.Translation{}, fmt.Errorf("TranslationUseCase - Translate - s.webAPI.Translate: %w", err)
+//	}
+//
+//	err = uc.repo.Store(context.Background(), translation)
+//	if err != nil {
+//		return entity.Translation{}, fmt.Errorf("TranslationUseCase - Translate - s.repo.Store: %w", err)
+//	}
+//
+//	return translation, nil
+//}
