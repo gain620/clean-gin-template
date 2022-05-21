f39e6f692a06b9bebafde48a4bc404b2af2b34e3
muon <gainchang620@gmail.com>
2022-05-22 02:16:10 +0900

    fix: Update user

--- internal/model/user/user.go
+++ internal/model/user/user.go
@@ -5,7 +5,8 @@ import "time"
 // User Entity -.
 type User struct {
 	ID        int64     `json:"id"`
-	Name      string    `json:"name" validate:"required"`
+	UserName  string    `json:"username" validate:"required"`
+	Email     string    `json:"email" validate:"required"`
 	Age       int64     `json:"age"`
 	UpdatedAt time.Time `json:"updated_at"`
 	CreatedAt time.Time `json:"created_at"`
