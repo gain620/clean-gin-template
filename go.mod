c5b3c8241e1f5ccfb765fc88027ad089fe220828
muon <gainchang620@gmail.com>
2022-05-24 20:48:45 +0900

    feat: Change the jwt dependency to Square's jwt package

--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/swaggo/files v0.0.0-20210815190702-a29dd2bc99b2
 	github.com/swaggo/gin-swagger v1.3.3
 	go.elastic.co/apm/module/apmgin v1.15.0
+	gopkg.in/square/go-jose.v2 v2.6.0
 )
 
 require (
--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -1,8 +1,10 @@
 package jwt
 
 import (
+	"clean-gin-template/pkg/logger"
 	"fmt"
-	"github.com/dgrijalva/jwt-go"
+	"gopkg.in/square/go-jose.v2/jwt"
+	"strings"
 )
 
 type JwtValidator interface {
@@ -10,6 +12,7 @@ type JwtValidator interface {
 }
 
 type jwtValidator struct {
+	l logger.Interface
 }
 
 type jwtCustomClaims struct {
@@ -17,19 +20,44 @@ type jwtCustomClaims struct {
 	Admin bool   `json:"admin"`
 }
 
-func New() JwtValidator {
-	return &jwtValidator{}
+func New(l logger.Interface) JwtValidator {
+	return &jwtValidator{
+		l: l,
+	}
 }
 
 func (j jwtValidator) Validate(tokenStr string) (*jwt.Token, error) {
-	return jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+	//return jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+	//
+	//	// Signing method validation
+	//	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+	//		return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+	//	}
+	//
+	//	// Return the secret signing key
+	//	//return []byte(jwtSrv.secretKey), nil
+	//})
+}
 
-		// Signing method validation
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+func (j jwtValidator) CheckJwtRole(tokenStr, roleName string) (bool, error) {
+	var claims JwtPayload // custom payload struct to store parsed token
+
+	// decode JWT token without verifying the signature
+	token, _ := jwt.ParseSigned(tokenStr)
+	_ = token.UnsafeClaimsWithoutVerification(&claims)
+
+	for _, item := range claims.Roles {
+		_, jwtRole, ok := strings.Cut(item, "_")
+		if ok != true {
+			j.l.Fatal("the format does not fit")
+			return false, fmt.Errorf("the role format does not fit")
 		}
 
-		// Return the secret signing key
-		return []byte(jwtSrv.secretKey), nil
-	})
+		if jwtRole == roleName {
+			j.l.Print("ACCESS ALLOWED ROLE")
+			return true, nil
+		}
+	}
+
+	return false, nil
 }
